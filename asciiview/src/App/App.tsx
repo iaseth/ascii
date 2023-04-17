d53936f40ca5f6d3304defbd60af22d3076166e0
Ankur Seth <iankurseth@gmail.com>
2023-04-17 08:28:08 +0530

    Installed miniwindcss, reorganized src and added tailwind

---
+++ asciiview/src/App/App.tsx
@@ -0,0 +1,15 @@
+import 'miniwindcss';
+import './App.css';
+
+
+
+function App () {
+
+	return (
+		<div className="min-h-screen flex bg-red-200 text-red-800">
+			<h1 className="m-auto">ASCII View</h1>
+		</div>
+	)
+}
+
+export default App;

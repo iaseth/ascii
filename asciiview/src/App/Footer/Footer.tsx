f70f00673aabb0e9a9c4cbcb414b3025bcb729c7
Ankur Seth <iankurseth@gmail.com>
2023-04-19 19:12:24 +0530

    Installed dom-to-image

--- asciiview/src/App/Footer/Footer.tsx
+++ asciiview/src/App/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer () {
 	return (
-		<footer className="bg-zinc-300 text-center px-4 py-16">
+		<footer className="bg-zinc-100 text-center px-4 py-16 border-t-2 border-slate-500">
 			<h4>Created by <a href="https://github.com/iaseth" className="link">Ankur Seth</a></h4>
 		</footer>
 	);
